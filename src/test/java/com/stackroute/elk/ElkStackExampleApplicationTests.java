57ad4cb3fe8d18da2c2071bf05d455f99a2e2da4
saravanan.1.p <saravanan.1.p@niit.com>
2024-12-11 12:09:41 +0530

    soln

---
+++ src/test/java/com/stackroute/elk/ElkStackExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkStackExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
